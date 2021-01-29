5fae6c960361904b54eb78048fa56e01b933ae26
nicolasform <nicolas.form@gmail.com>
2021-01-30 05:45:44 +0900

    Add missing closing for meta viewport

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<title>Letter O or digit 0 - Copy/paste your text to know</title>
 	       
 	<style>
